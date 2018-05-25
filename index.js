61619d33f2324d3f9dd9a0f514fa92e2a0f17475
Mike Davies <miked@antfx.com>
2018-05-25 10:07:59 +0200

    update

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var fs = require('fs');
 var pdf = require('html-pdf');
 var html = fs.readFileSync('./input.html', 'utf8');
-var options = { format: 'Letter' };
+
+const options = {
+  format: 'A3',
+  base: `./`,
+  border: {
+    top: '1in',
+    bottom: '1in',
+  },
+  timeout: 120000
+};
 
 pdf.create(html, options).toFile('./output.pdf', function(err, res) {
-  if (err) return console.log(err);
-  console.log(res); // { filename: '/app/businesscard.pdf' }
+  if (err)
+    return console.log(err);
+  console.log(res);
 });
